21a57bf0e64f57b93afe54cc34bac7073018db9d
Killedar38 <q.fombaron@hotmail.fr>
2017-11-24 16:39:39 +0100

    Add List, Search

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,72 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ResolvedReflectiveFactory } from '@angular/core/src/di/reflective_provider';
+
+export interface Result {
+  author: string;
+  date: number;
+  image: string;
+  title: string;
+}
+
+const fakeResults: Result[] = [
+  {
+  author: 'Author1',
+  date: 2017,
+  //image: 'http://lorempixel.com/300/300/abstract/1',
+  image: 'http://via.placeholder.com/300x300',
+  title: 'Result1'
+  },
+  {
+  author: 'Author2',
+  date: 2018,
+  //image: 'http://lorempixel.com/300/300/abstract/2',
+  image: 'http://via.placeholder.com/300x300',
+  title: 'Result2'
+  },
+  {
+  author: 'Author3',
+  date: 2019,
+  //image: 'http://lorempixel.com/300/300/abstract/3',
+  image: 'http://via.placeholder.com/300x300',
+  title: 'Result3'
+  }
+]
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  results: Result[];
+  searchQuery: string = '';
+  items: Result[];
 
   constructor(public navCtrl: NavController) {
+    this.initializeItems([]);
+  }
 
+  initializeItems(results: Result[]): void {
+    this.results = results;
+  }
+
+  //Fonction de recherche
+  getItems(ev: any) {
+    // this.initializeItems();
+    let val = ev.target.value;
+    // if (val && val.trim() != '') {
+    //   this.results = this.results.filter((result) => {
+    //     return (result.title.toLowerCase().indexOf(val.toLowerCase()) > -1
+    //     || result.author.toLowerCase().indexOf(val.toLowerCase()) > -1
+    //     || result.date.toString().indexOf(val.toString()) > -1);
+    //   })
+    // }
+    if (val) {
+     this.initializeItems(fakeResults);
+    } else {
+      this.initializeItems([]);
+    }
   }
 
 }
